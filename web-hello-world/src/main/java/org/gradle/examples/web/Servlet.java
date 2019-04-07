810317ca199c29b4455c002b43bc0add26b6cbb3
msharma1 <manish.sharma0201cs@gmail.com>
2019-04-07 20:47:50 +0530

    Fixing compilation errpr
    [web-hello-world] $ /var/lib/jenkins/workspace/gradle_test_pipeline/web-hello-world/gradlew clean wrapper build
    :clean
    :wrapper UP-TO-DATE
    :compileJava/var/lib/jenkins/workspace/gradle_test_pipeline/web-hello-world/src/main/java/org/gradle/examples/web/Servlet.java:8: error: class, interface, or enum expected
    package junit;
    ^
    1 error
     FAILED
    
    FAILURE: Build failed with an exception.
    
    * What went wrong:
    Execution failed for task ':compileJava'.
    > Compilation failed; see the compiler error output for details.

--- web-hello-world/src/main/java/org/gradle/examples/web/Servlet.java
+++ web-hello-world/src/main/java/org/gradle/examples/web/Servlet.java
@@ -1,11 +1,11 @@
 package org.gradle.examples.web;
+package junit;
 
 import java.io.PrintWriter;
 import java.io.IOException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-package junit;
  
 public class JUnitHelloWorld {
  
