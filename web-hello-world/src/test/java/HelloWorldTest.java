db66dab2eb95f0c78c3d62a9587cf47a26bbe5f4
msharma1 <manish.sharma0201cs@gmail.com>
2019-04-28 19:53:02 +0530

    Giving a print statement

--- web-hello-world/src/test/java/HelloWorldTest.java
+++ web-hello-world/src/test/java/HelloWorldTest.java
@@ -6,6 +6,7 @@ public class HelloWorldTest {
   public void HelloWorldTest() {
       HelloWorld h = new HelloWorld();
       String str = "World";
+      System.out.println("Test called" );
  
       assertEquals(str, h.Hello(str));
   }
